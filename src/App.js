a935bded35920fb56bd0f7f4d090725e514a98c4
kckc4u <vinodvaishnav@MacBook-Pro.local>
2020-10-05 11:38:25 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import {Switch, Route} from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import Shoppage from './pages/shoppage/shoppage.component';
+import Header from './components/header/header.component';
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Homepage} />
-      <Route exact path="/shop" component={Shoppage} />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={Shoppage} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './header.style.scss';
+
+import {Link} from 'react-router-dom';
+import Logo from '../../assets/crown.svg';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Link to="/" className="logo-container">
+                <img src={Logo} alt="Lions" className="logo"/>
+            </Link>
+            <div className="options">
+                <Link className="option" to="/shop">Shop</Link>
+                <Link className="option" to="/shop">Contact</Link>  
+            </div>
+        </div>
+    )
+}
+
+export default Header;
